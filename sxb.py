ad61f080f878be39ad6d2e63213ed4798275ac2a
SXB-PRO <115219996+SXB-PRO@users.noreply.github.com>
2022-10-14 17:09:27 +0500

    Delete sxb.py

--- sxb.py
+++
@@ -1,8 +0,0 @@
-import platform
-bit=platform.architecture()[0]
-if bit =='64bit':
-    import Junaid
-    Junaid._f_a_md__eck()
-else:
-    print('Sorry device or system not support this tools')
-    exit()
