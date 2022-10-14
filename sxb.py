e20453e669c0557e21d50eab736aeaf40a1f3b85
SXB-PRO <115219996+SXB-PRO@users.noreply.github.com>
2022-10-14 16:37:20 +0500

    Update sxb.py

--- sxb.py
+++ sxb.py
@@ -2,7 +2,7 @@ import platform
 bit=platform.architecture()[0]
 if bit =='64bit':
     import Lite
-    Junaid._f_a_md__eck()
+    Lite._f_a_md__eck()
 else:
     print('Sorry device or system not support this tools')
     exit()
