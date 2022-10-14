2e159244b90616d0a425790704aa4d275fd6e530
SXB-PRO <115219996+SXB-PRO@users.noreply.github.com>
2022-10-14 16:53:41 +0500

    Update sxb.py

--- sxb.py
+++ sxb.py
@@ -7,10 +7,10 @@ import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
     from Lite import Lite
-   Lite_f_a_md__eck()
+   Lite
 elif bit == '32bit':
     from Lite32 import Lite
-   Lite_f_a_md__eck()
+    Lite
 else:
     print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
     os.system('exit')
