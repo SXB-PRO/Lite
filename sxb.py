af591f709b17987b1d80c388df3d8915064946e6
SXB-PRO <115219996+SXB-PRO@users.noreply.github.com>
2022-10-14 16:26:05 +0500

    Update and rename SXB-Lite.py to sxb.py

--- sxb.py
+++ sxb.py
@@ -2,7 +2,7 @@ import platform
 bit=platform.architecture()[0]
 if bit =='64bit':
     import Lite
-    Lite.main_apv()
+    Junaid._f_a_md__eck()
 else:
     print('Sorry device or system not support this tools')
     exit()
